0c22bb21a1d2d51b3e271bcd770c516720dd7150
Ayush_God <ayushgodbole31@gmail.com>
2024-07-04 21:36:53 +0530

    set matrix zero better

--- Array/p1.cpp
+++ Array/p1.cpp
@@ -1,46 +1,30 @@
 //  SET MATRIX ZERO
 
+// BETTER
 
 class Solution {
 public:
-
-    void makeRow(int row , vector<vector<int>> &mat){
-        int m = mat[0].size();
-        for(int j=0; j<m; j++){
-            if(mat[row][j]!=0){
-                mat[row][j] = -1;
-            }
-        }
-    }
-
-    void makeCol(int col , vector<vector<int>> &mat){
-        int n = mat.size();
-        for(int j=0; j<n; j++){
-            if(mat[j][col]!=0){
-                mat[j][col] = -1;
-            }
-        }
-    }
-
     void setZeroes(vector<vector<int>>& matrix) {
+        // better
 
         int n = matrix.size();
         int m = matrix[0].size();
 
-        // brute force
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
-                    makeRow(i,matrix);
-                    makeCol(j,matrix);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        // replace -1 to 0
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j]==-1){
+                if(row[i]==1 || col[j]==1){
                     matrix[i][j] = 0;
                 }
             }
@@ -48,5 +32,5 @@ public:
     }
 };
 
-// TC : O(n^3)
-// SC : O(1)
+// TC : O(n*m) + O(n*m)
+// SC : O(n) + O(m)
