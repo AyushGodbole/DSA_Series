ad32389c0e44262f91599bf50b356a56b1ab1868
Ayush_God <ayushgodbole31@gmail.com>
2024-07-05 19:14:01 +0530

    sort colors optimal - DNF algo

--- Array/p5.cpp
+++ Array/p5.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero = 0;
-        int one = 0;
-        int two = 0;
+        // OPTIMAl - Dutch National Flag algo (DNF)
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==0) zero++;
-            else if (nums[i]==1) one++;
-            else two++;
-        }
-
-        int i = 0;
         int n = nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
 
-        while(i<n){
-            if(zero-->0){
-                nums[i] = 0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            else if(one-->0){
-                nums[i] = 1;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                nums[i] = 2;
-                two--;
+                swap(nums[mid],nums[high]);
+                high--;
             }
-            i++;
         }
     }
 };
 
-// TC : O(n) + O(n)
+// TC : O(N)
 // SC : O(1)
