48e1b58d15df06070354caff7ed55472de481f9e
Ayush_God <ayushgodbole31@gmail.com>
2024-07-06 22:02:24 +0530

    merge sorted arrays optimal-1

--- Array-part2/p3.cpp
+++ Array-part2/p3.cpp
@@ -1,41 +1,13 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // BETTER
-        int i=0;
-        int j=0;
+        // OPTIMAL
 
-        vector<int> ans;
-
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                ans.push_back(nums2[j]);
-                j++;
-            }
+        for(int j=0,i=m; j<n; j++){
+            nums1[i++] = nums2[j];
         }
-
-        // nums1 left
-        while(i<m){
-            ans.push_back(nums1[i]);
-            i++;
-        }
-
-        // nums2 left
-        while(j<n){
-            ans.push_back(nums2[j]);
-            j++;
-        }
-
-        // copying ans to nums1
-        for(int k=0; k<m+n; k++){
-            nums1[k] = ans[k];
-        }
+        sort(nums1.begin(),nums1.end());
     }
 };
 
-// TC : O(N+M) + O(M) + O(N)
-// SC : O(N+M)
+// TC : O(n) + O(mlog(m+n))
