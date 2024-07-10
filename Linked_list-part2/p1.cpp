bf970c5b6582b8968afb84fa2ea25e86a8fb39bc
Ayush_God <ayushgodbole31@gmail.com>
2024-07-10 20:54:01 +0530

    Find intersection of Two node , optimal

--- Linked_list-part2/p1.cpp
+++ Linked_list-part2/p1.cpp
@@ -7,27 +7,54 @@
  * };
  */
 class Solution {
-public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // better
-
-        // storing each node(address) into map
-        unordered_map<ListNode *,bool> visited;
-        ListNode *temp = headA;
+private:
+    int findLen(ListNode* head){
+        int len=0;
+        ListNode* temp = head;
         while(temp!=NULL){
-            visited[temp] = true;
+            len++;
             temp = temp->next;
         }
 
-        temp = headB;
-        while(temp!=NULL){
-            if(visited[temp]) return temp;
-            temp = temp->next;
+        return len;
+    }
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // OPTIMAL
+
+        // 1. find lengths
+        int len1 = findLen(headA);
+        int len2 = findLen(headB);
+
+        // 2. find difference
+        int diff = abs(len1-len2);
+        ListNode* temp1=NULL;
+        ListNode* temp2 = NULL;
+        // temp1 points to larger length list
+        if(len1>len2){
+            temp1 = headA;
+            temp2 = headB;
+        }
+        else{
+            temp1 = headB;
+            temp2 = headA;
+        }
+
+        // 3. move larger list by diff
+        while(diff--){
+            temp1 = temp1->next;
+        }
+
+        // 4. now traverse both by 1
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1==temp2) return temp1;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
         }
 
         return NULL;
     }
 };
 
-// TC : O(N+M)
-// SC : O(N)
+// TC : O(N1) + O(N2) + O(Diff)
+// SC : O(1)
