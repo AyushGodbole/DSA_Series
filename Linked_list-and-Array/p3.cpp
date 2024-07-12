d03bc9e05ec591fb6313fb64e7d5235d9ac3a1a7
Ayush_God <ayushgodbole31@gmail.com>
2024-07-12 19:32:22 +0530

    3 Sum , Better , calculating third element by -(nums[i]+nums[j])

--- Linked_list-and-Array/p3.cpp
+++ Linked_list-and-Array/p3.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // BRUTE FORCE - checking each 
+        // BETTER - calculating third by -(nums[i]+nums[j])
 
-        int n = nums.size();
-        // set to store unique elements
+        int n=nums.size();
         set<vector<int>> st;
         for(int i=0; i<n; i++){
+            set<int> hashMp;
             for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if(nums[i]+nums[j]+nums[k]==0){
-                        vector<int> temp = {nums[i],nums[j],nums[k]};
-                        sort(temp.begin(),temp.end());
-
-                        st.insert(temp);
-                    }
+                int third = -(nums[i]+nums[j]);
+                // if third present in hashmap
+                if(hashMp.find(third)!=hashMp.end()){
+                    vector<int> temp = {nums[i],nums[j],third};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
                 }
+                hashMp.insert(nums[j]);
             }
         }
 
@@ -24,5 +24,5 @@ public:
     }
 };
 
-// TC : O(N^3)
+// TC : O(N^2)
 // SC : O(N)
