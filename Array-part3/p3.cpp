78c3c8e519d635e97b78e464eab2b3054e7bf192
Ayush_God <ayushgodbole31@gmail.com>
2024-07-13 19:16:54 +0530

    majority element |_ n/2 _| , brute

---
+++ Array-part3/p3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // BRUTE FORCE
+
+        unordered_map<int,int> mp;
+        for(auto ele:nums){
+            mp[ele]++;
+        }
+
+        int n=nums.size();
+        int majority=n/2;
+        for(auto it:mp){
+            if(it.second > majority) return it.first;
+        }
+
+        return -1;
+    }
+};
+
+// TC : O(N) + O(N)
+// SC : O(1)
