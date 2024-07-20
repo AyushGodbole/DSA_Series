8e6af6cdc6a926bc8c819bfd536a4c2edd4e74fc
Ayush_God <ayushgodbole31@gmail.com>
2024-07-20 14:08:22 +0530

    Lowest common ancestor optimal

--- Binary_trees-part2/p4.cpp
+++ Binary_trees-part2/p4.cpp
@@ -8,54 +8,26 @@
  * };
  */
 class Solution {
-private:
-    bool findPath(TreeNode* root, vector<TreeNode*> &path, TreeNode* node){
-        if(root==NULL) return false;
-
-        // include in path
-        path.push_back(root);
-
-        // check if element found
-        if(root->val==node->val) return true;
-
-        // left and right calls
-        bool left = findPath(root->left,path,node);
-        bool right = findPath(root->right,path,node);
-
-        if(left==true || right==true) return true;
-
-        path.pop_back();
-        return false;
-    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // BRUTE FORCE
-        // find path from root to node 1, node 2 and compare them
+        // OPTIMAL
 
-        vector<TreeNode*> path1;
-        vector<TreeNode*> path2;
+        if(root==NULL || root==p || root==q) return root;
 
-        bool f1 = findPath(root,path1,p);
-        bool f2 = findPath(root,path2,q);
+        // left call
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        // right call
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
 
-        int i=0;
-        int j=0;
-        TreeNode* prev = NULL;
-
-        while(i<path1.size() && j<path2.size()){
-            if(path1[i]==path2[j]){
-                prev = path1[i];
-            }
-            else{
-                break;
-            }
-            i++;
-            j++;
+        // cases
+        if(left==NULL) return right;
+        else if(right==NULL) return left;
+        else{
+            // means both are non-null , hence the node where we are standing is the LCA
+            return root;
         }
-
-        return prev;
     }
 };
 
-// TC : O(N) + O(N)
-// SC : O(N) + O(N) + O(N) + O(N)
+// TC : O(N)
+// SC : O(N)
