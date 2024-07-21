eb976531d997fff542803f6b00b39542d08cc9b3
Ayush_God <ayushgodbole31@gmail.com>
2024-07-21 18:45:03 +0530

    Symmetric Tree , OPTIMAL

--- Binary_trees-part3/p4.cpp
+++ Binary_trees-part3/p4.cpp
@@ -11,82 +11,33 @@
  */
 class Solution {
 private:
-    void traverseLeft(TreeNode* root, vector<TreeNode*> &left) {
-        if (root == nullptr) {
-            left.push_back(nullptr);
-            return;
+    bool check(TreeNode* leftPtr, TreeNode* rightPtr){
+        if(leftPtr==NULL || rightPtr==NULL){
+            if(leftPtr==rightPtr) return true;
+            return false;
         }
 
-        left.push_back(root);
+        if(leftPtr->val != rightPtr->val) return false;
 
-        // left
-        traverseLeft(root->left, left);
+        // check for leftPtr->left and rightPtr->right
+        bool c1 = check(leftPtr->left,rightPtr->right);
 
-        // right
-        traverseLeft(root->right, left);
-    }
+        // check for leftPtr->right and rightPtr->left
+        bool c2 = check(leftPtr->right,rightPtr->left);
 
-    void traverseRight(TreeNode* root, vector<TreeNode*> &right) {
-        if (root == nullptr) {
-            right.push_back(nullptr);
-            return;
-        }
+        if(c1==true && c2==true) return true;
 
-        right.push_back(root);
-
-        // right
-        traverseRight(root->right, right);
-        
-        // left
-        traverseRight(root->left, right);
+        return false;
     }
 public:
     bool isSymmetric(TreeNode* root) {
-        // BRUTE FORCE
-        vector<TreeNode*> left;
-        vector<TreeNode*> right;
+        // OPTIMAL - one go left , one go right
+        if(root==NULL) return true;
 
-        traverseLeft(root->left, left);
-        traverseRight(root->right, right);
-
-        for (auto ele : left) {
-            if (ele != nullptr) {
-                cout << ele->val << " ";
-            } else {
-                cout << "null ";
-            }
-        }
-        cout << endl;
-
-        for (auto ele : right) {
-            if (ele != nullptr) {
-                cout << ele->val << " ";
-            } else {
-                cout << "null ";
-            }
-        }
-        cout << endl;
-
-        int i = 0;
-        int j = 0;
-        while (i < left.size() && j < right.size()) {
-            if (left[i] == nullptr && right[j] == nullptr) {
-                // Both are null, continue
-                i++;
-                j++;
-            } else if (left[i] == nullptr || right[j] == nullptr) {
-                // One is null and the other is not, not symmetric
-                return false;
-            } else if (left[i]->val != right[j]->val) {
-                // Values are different
-                return false;
-            } else {
-                // Values are the same, continue
-                i++;
-                j++;
-            }
-        }
-
-        return true;
+        bool ans = check(root->left,root->right);
+        return ans;
     }
 };
+
+// TC : O(N)
+// SC : O(N)
