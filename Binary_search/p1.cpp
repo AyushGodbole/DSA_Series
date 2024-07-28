f66d20d2e41170b5506453203e691a0312f1ac1c
Ayush_God <ayushgodbole31@gmail.com>
2024-07-28 19:08:52 +0530

    Binary Seach recursive

--- Binary_search/p1.cpp
+++ Binary_search/p1.cpp
@@ -1,18 +1,19 @@
 class Solution {
+private:
+    int solve(int low, int high, vector<int> &nums, int target){
+        if(low>high) return -1;
+
+        int mid = low + (high-low)/2;
+
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]<target) return solve(mid+1,high,nums,target);
+        else return solve(low,mid-1,nums,target);
+    }
 public:
     int search(vector<int>& nums, int target) {
-        // iterative
-        int low = 0;
-        int high = nums.size()-1;
+        // recursive
 
-        while(low<=high){
-            int mid = low + (high-low)/2;
-
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]<target) low = mid+1;
-            else high = mid-1;
-        }
-
-        return -1;
+        int n=nums.size();
+        return solve(0,n-1,nums,target);
     }
 };
